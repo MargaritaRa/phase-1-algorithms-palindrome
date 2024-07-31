27369e40c4d13b2c870a402d0080da0cbf55553e
Margarita Ramirez <margarita26.mr@gmail.com>
2024-07-31 15:12:07 -0400

    completed

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function isPalindrome(word) {
+function reverseString(word) {
   // Write your algorithm here
   let reverse = word.split('').reverse().join('')
     return reverse
+}
+
+function isPalindrome(word) {
+  // reverse the input string
+  const reverseWord = reverseString(word);
+  if (word === reverseWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -19,6 +29,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
